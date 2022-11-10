4381905b775edcc668c5fee1ec37e7513a9dfad7
admin <xiaojie.bai@daxiangyun.com>
2022-11-10 13:32:25 +0800

    列表

--- demo.py
+++ demo.py
@@ -1,3 +1,20 @@
-def demo():
-    print("hello,world")
-demo()
+import time
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+
+
+def showitems(args):
+    time.sleep(5)
+    for item in args:
+        if item == 'redline':
+            print(item.lower())
+        elif item == 'trek':
+            print(item.upper())
+        else:
+            print(item.title())
+start = time.time()
+print("Start:{0}".format(start))
+showitems(bicycles)
+end = time.time()
+print("End:{0}".format(end))
+run = end - start
+print("run_time:{0}".format(run))
