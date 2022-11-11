700ede2ddc87165ee9cbfb53cd4a404f07d99f98
admin <xiaojie.bai@daxiangyun.com>
2022-11-11 14:30:16 +0800

    列表-修改/增加/删除

--- demo.py
+++ demo.py
@@ -1,6 +1,25 @@
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+##################列表#######################################
 import time
 bicycles = ['trek', 'cannondale', 'redline', 'specialized']
-
 
 def showitems(args):
     time.sleep(5)
@@ -18,3 +37,22 @@ end = time.time()
 print("End:{0}".format(end))
 run = end - start
 print("run_time:{0}".format(run))
+
+#修改/添加/删除元素
+bicycles = ['trek', 'cannondale', 'redline', 'specialized']
+def m(args):
+    bicycles[0] = 'kert'
+    print(args)
+def a(args):
+    args.append('Dog')
+    args.insert(2, 'insert')
+    print(args)
+def d(args):
+    del args[1]
+    print(args)
+    args.pop()  #默认最后一个元素,指定索引即可删除指定索引的元素,删除的元素需要使用
+    print(args)
+
+m(bicycles)
+a(bicycles)
+d(bicycles)
